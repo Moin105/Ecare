5d103cbd2a24726fc78f33b0ca2eea97937fe33a
Moin105 <moinlatif105@gmail.com>
2022-09-10 22:13:42 +0500

    responsiveness fixes

--- Components/Getstarted.js
+++ Components/Getstarted.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 import kali from '../public/kali.png'
 function Getstarted() {
   return (
-    <div className={styles.getstarted}>
+    <div className={styles.get}>
+      <div className={styles.getstarted}>
         <figure className={styles.figure}>
         <Image
                     src={kali}
@@ -16,12 +17,13 @@ function Getstarted() {
         <div className={styles.transparent}>
             <div className={styles.left}><h2 className={styles.h2}>Connect<br></br> with Your patients from anywhere!</h2></div>
             <div className={styles.right}><p className={styles.p}>Get more appointments through online bookings
-Create and view patient records from anywhere
-Manage your schedule efficiently.</p>
-<button className={styles.button}>Get started</button>
-</div>
+            Create and view patient records from anywhere
+            Manage your schedule efficiently.</p>
+        <button className={styles.button}>Get started</button>
+        </div>
            
         </div>
+    </div>
     </div>
   )
 }
