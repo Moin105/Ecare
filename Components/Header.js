3ae55899a3bde138797a3d24f2a38f0fbf14c3d5
Moin105 <moinlatif105@gmail.com>
2022-09-11 19:20:20 +0500

    translation

--- Components/Header.js
+++ Components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
 import logo from '../public/logo.png'
-import Image from "next/image";
+import Image from "next/image"
+import { useRouter } from 'next/router';
 
+import en from "../Locales/en"
+import fr from "../Locales/fr"
 
 function Header() {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === 'en' ?   en : fr;
+
+  const changeLanguage = (e) => {
+    const locale = e.target.value;
+    router.push(router.pathname, router.asPath, { locale });
+  };
   return (
     <header className={styles.header}>
    <figure className={styles.logo}>
@@ -17,13 +28,22 @@ function Header() {
         </figure>
     <div className={styles.nav}>
         <ul>
-            <li>Home</li>
-            <li>About</li>
+            <li>Home{t.subtitle}</li>
+            <li>{t.about}</li>
             <li>Mobile Apps</li>
             <li>Features</li>
         </ul>
     </div>
-    <div className={styles.langc}><p>En</p></div>
+    <div className={styles.langc}><p>
+    <select
+            onChange={changeLanguage}
+            defaultValue={locale}
+            className="text-white text-shadow-sm text-lg bg-transparent tracking-wide"
+          >
+            <option className="text-black" value="en">EN</option>
+            <option className="text-black" value="fr">FR</option>
+          </select>
+      </p></div>
 
     </header>
   )
