dffae804191a352fce87e3fc5e989d11953eac2c
Moin105 <moinlatif105@gmail.com>
2022-09-11 21:22:54 +0500

    context translation

--- Components/Context.js
+++ Components/Context.js
@@ -3,15 +3,24 @@ import styles from '../styles/Home.module.css'
 import google from '../public/google.png'
 import Image from "next/image";
 import apple from '../public/apple.png'
+import { useRouter } from 'next/router';
+
+import en from "../Locales/en"
+import fr from "../Locales/fr"
 function Context() {
+  const router = useRouter();
+  const { locale } = router;
+  const t = locale === 'en' ?   en : fr;
   return (
     <div className={styles.context}>
         <h2 className={styles.h2}>
-          Automated Solution<br></br> for Your Healthcare Needs!
+          {t.bh}
+          {/* Automated Solution<br></br> for Your Healthcare Needs! */}
         </h2>
         <p className={styles.subtext}>
-        The health, safety and well-being of our members, employees<br></br> 
-        and the communities we serve is our top priority. 
+        {/* The health, safety and well-being of our members, employees<br></br> 
+        and the communities we serve is our top priority.  */}
+        {t.cp}
         </p>
         <div className={styles.links}>
         <figure className={styles.figure}>
