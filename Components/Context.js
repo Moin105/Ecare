55b48c4225058b3dbb40a979db0296de0488a1ef
Moin105 <moinlatif105@gmail.com>
2022-09-19 17:45:35 +0500

    footer fixed

--- Components/Context.js
+++ Components/Context.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import styles from '../styles/Home.module.css'
 import google from '../public/google.png'
 import Image from "next/image";
@@ -9,8 +9,26 @@ import en from "../Locales/en"
 import fr from "../Locales/fr"
 function Context() {
   const router = useRouter();
+  useEffect(() => {
+    fetch('http://www.ecareserve.com/api/show_links') 
+    .then((res) => {
+      if (res.status == 200) {
+        return res.json();
+      }else{
+        console.log("res");
+      }
+    })
+      .then((res) => {
+        console.log(res);
+      //   setResponse(res.message);
+        console.log("safa",res);
+      });
+  
+  }, [])
+  
   const { locale } = router;
   const t = locale === 'en' ?   en : fr;
+  // console.log("SADAPAY",process.env.REACT_APP_API_KEY)
   return (
     <div className={styles.context} id='context'>
         <h2 className={styles.h2}>
