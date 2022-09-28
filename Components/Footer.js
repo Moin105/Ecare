84b755550a2b48f420e37e4e4f309bbfc2f2de3f
Moin105 <moinlatif105@gmail.com>
2022-09-28 17:52:28 +0500

    fixes

--- Components/Footer.js
+++ Components/Footer.js
@@ -3,6 +3,8 @@ import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router';
 import en from "../Locales/en"
 import fr from "../Locales/fr"
+import Link from "next/link";
+
 function Footer() {
   const router = useRouter();
   const { locale } = router;
@@ -28,19 +30,21 @@ function Footer() {
                 <button className={styles.button}>{t.send}</button>
               </div>
               </div>
-              <ul className={styles.list}>
-                <li className={styles.li}>About Us</li>
-                <li className={styles.li}>Android App</li>
-                <li className={styles.li}>iOS App</li>
+              <ul className={styles.list}>  
+              <li className={styles.li}><Link href="/#about" smooth="true">{t.about}</Link></li>          
+             
+              <li className={styles.li}><Link href="/#features" smooth="true">Features</Link></li>
+              <li className={styles.li}><Link href="/#contact" smooth="true">Contact</Link></li> 
+              <li className={styles.li}><Link href="/#why" smooth="true">{t.features}</Link></li>
+                {/* <li className={styles.li}>About Us</li>
                 <li className={styles.li}>How it works?</li>
                 <li className={styles.li}>Features</li>
-                <li className={styles.li}>Why us?</li>
+                <li className={styles.li}>Why us?</li> */}
               </ul>
               <ul className={styles.list2}>
-                <li className={styles.li}>Facebook</li>
-                <li className={styles.li}>Twitter</li>
-                <li className={styles.li}>Instagram</li>
-                <li className={styles.li}>LinkedIn</li>
+              <a href='https://www.facebook.com/profile.php?id=100086304606356'> <li className={styles.li}>Facebook</li></a>
+              <a href="https://twitter.com/ecare_med" >  <li className={styles.li}>Twitter</li></a>
+              <a href="https://www.instagram.com/ecareDocta/"><li className={styles.li}>Instagram</li></a>
               </ul>
         </div>
         <div className={styles.bfooter}>{t.cr}</div>
